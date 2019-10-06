9a6f900b5bc7a68e987ea1e2d9c7420bec013ee2
Margarita <ricunsos@gmail.com>
2019-10-06 13:36:21 +0300

    Update Form1.cs

--- ConvexHull/ConvexHull/Form1.cs
+++ ConvexHull/ConvexHull/Form1.cs
@@ -27,9 +27,27 @@ namespace ConvexHull
             g.Clear(Color.White);
         }
 
+        //рисуем заданные точки и добавляем их в массив точек
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            points.Add(e.Location);
+            g.FillEllipse(brush, e.Location.X - 3, e.Location.Y - 3, 7, 7);
+            pictureBox1.Invalidate();
+        }
+
         private void buildBtn_Click(object sender, EventArgs e)
         {
-
+            int n = points.Count;
+            if (n <= 5)
+                bruteHull(points);
+            else
+            {
+                List<Point> leftHull, rightHull;
+                leftHull = points.GetRange(0, n / 2);
+                rightHull = points.GetRange(n /2 , n/2);
+                bruteHull(leftHull);
+                bruteHull(rightHull);
+            }
         }
 
         //очищаем экран и обнуляем список
@@ -40,12 +58,51 @@ namespace ConvexHull
             pictureBox1.Invalidate();
         }
 
-        //рисуем заданные точки и добавляем их в массив точек
-        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+
+
+        //алгоритм построения оболочки для малого количества точек
+        private void bruteHull(List<Point> points)
         {
-            points.Add(e.Location);
-            g.FillEllipse(brush, e.X - 3, e.Y - 3, 7, 7);
-            pictureBox1.Invalidate();
+            bool allPointsOnTheRight;
+            Point pti, ptj;
+            for (int i = 0;i < points.Count; i++)
+            {
+                for(int j = 0; j < points.Count; j++)
+                {
+                    if (i == j)
+                        continue;
+
+                    pti = points[i];
+                    ptj = points[j];
+
+                    allPointsOnTheRight = true;
+
+                    for (int k = 0; k < points.Count; k++)
+                    {
+                        if (k == i || k == j)
+                            continue;
+                        int side = findPointPos(points[k],pti,ptj);
+                        if(side > 0)
+                        {
+                            allPointsOnTheRight = false;
+                            break;
+                        }
+                    }
+
+                    if(allPointsOnTheRight)
+                    {
+                        g.DrawLine(pen,pti,ptj);
+                        pictureBox1.Invalidate();
+                    }
+                }
+            }
+        }
+
+        //0 - принадлежит линии, > 0 - левее, < 0 - правее
+        int findPointPos(PointF p, Point A, Point B)
+        {
+            float result = (B.X - A.X) * (p.Y - B.Y) - (B.Y - A.Y) * (p.X - B.X);
+            return (int)result;
         }
     }
 }
